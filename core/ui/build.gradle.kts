c95a6c013cf8292389a7ca53e5291adc34a7c59c
Sp-I-dE-x <160456203+Sp-I-dE-x@users.noreply.github.com>
2024-07-21 14:09:05 +0530

    Add files via upload

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("xplayer.android.library")
+    id("xplayer.android.library.compose")
+}
+
+android {
+    namespace = "com.daljeet.xplayer.core.ui"
+}
+
+dependencies {
+
+    implementation(project(":core:model"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.bundles.compose)
+    implementation(libs.androidx.compose.material.iconsExtended)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.android.material)
+
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.compose.ui.test)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.testManifest)
+}
