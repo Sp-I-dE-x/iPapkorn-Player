3a2023bce832c2e460b1e723cbea31ab7fef1508
Sp-I-dE-x <160456203+Sp-I-dE-x@users.noreply.github.com>
2024-07-25 12:45:19 +0530

    Update OptionsDialog.kt

--- feature/settings/src/main/java/com/daljeet/xplayer/settings/composables/OptionsDialog.kt
+++ feature/settings/src/main/java/com/daljeet/xplayer/settings/composables/OptionsDialog.kt
@@ -1,22 +1,22 @@
-package com.daljeet.xplayer.settings.composables
+package dev.anilbeesetti.nextplayer.settings.composables
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.daljeet.xplayer.core.ui.components.CancelButton
-import com.daljeet.xplayer.core.ui.components.NextDialog
+import dev.anilbeesetti.nextplayer.core.ui.components.CancelButton
+import dev.anilbeesetti.nextplayer.core.ui.components.NextDialog
 
 @Composable
 fun OptionsDialog(
     text: String,
     onDismissClick: () -> Unit,
-    options: LazyListScope.() -> Unit
+    options: LazyListScope.() -> Unit,
 ) {
     NextDialog(
         onDismissRequest = onDismissClick,
@@ -24,14 +24,15 @@ fun OptionsDialog(
             Text(text = text)
         },
         content = {
-            Divider()
+            HorizontalDivider()
             LazyColumn(
                 contentPadding = PaddingValues(vertical = 8.dp),
-                modifier = Modifier.selectableGroup()
-            ) { options() }
-            Divider()
+                modifier = Modifier.selectableGroup(),
+                content = options,
+            )
+            HorizontalDivider()
         },
         dismissButton = { CancelButton(onClick = onDismissClick) },
-        confirmButton = { }
+        confirmButton = { },
     )
 }
