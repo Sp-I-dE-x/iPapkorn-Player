41ec3fc08b393f3264f31bf35dddd3c33bd68732
Sp-I-dE-x <160456203+Sp-I-dE-x@users.noreply.github.com>
2024-07-25 20:20:51 +0530

    Update DirectoryDao.kt

--- core/database/src/main/java/com/daljeet/xplayer/core/database/dao/DirectoryDao.kt
+++ core/database/src/main/java/com/daljeet/xplayer/core/database/dao/DirectoryDao.kt
@@ -2,8 +2,10 @@ package com.daljeet.xplayer.core.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
-import com.daljeet.xplayer.core.database.entities.DirectoryEntity
+import dev.anilbeesetti.nextplayer.core.database.entities.DirectoryEntity
+import dev.anilbeesetti.nextplayer.core.database.relations.DirectoryWithMedia
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -17,6 +19,10 @@ interface DirectoryDao {
 
     @Query("SELECT * FROM directories")
     fun getAll(): Flow<List<DirectoryEntity>>
+
+    @Transaction
+    @Query("SELECT * FROM directories")
+    fun getAllWithMedia(): Flow<List<DirectoryWithMedia>>
 
     @Query("DELETE FROM directories WHERE path in (:paths)")
     suspend fun delete(paths: List<String>)
