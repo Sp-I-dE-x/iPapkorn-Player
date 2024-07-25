bde7be2d94d4c01b5d1a6cc3e940ed2319ad0854
Sp-I-dE-x <160456203+Sp-I-dE-x@users.noreply.github.com>
2024-07-25 21:05:50 +0530

    Update Folder.kt

--- core/model/src/main/java/com/daljeet/xplayer/core/model/Folder.kt
+++ core/model/src/main/java/com/daljeet/xplayer/core/model/Folder.kt
@@ -16,7 +16,7 @@ data class Folder(
             mediaSize = 1024,
             mediaCount = 1,
             dateModified = 2000,
-            formattedMediaSize = "1KB"
+            formattedMediaSize = "1KB",
         )
     }
 }
