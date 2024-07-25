1b96a434265dcde02d6c4095dc171b05066fb7a5
Sp-I-dE-x <160456203+Sp-I-dE-x@users.noreply.github.com>
2024-07-25 11:04:35 +0530

    Update PermissionRationaleDialog.kt

--- app/src/main/java/com/daljeet/xplayer/ui/PermissionRationaleDialog.kt
+++ app/src/main/java/com/daljeet/xplayer/ui/PermissionRationaleDialog.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import com.daljeet.xplayer.core.ui.R
+import com.daljeet.xplayer.R
 import com.daljeet.xplayer.core.ui.preview.DayNightPreview
 import com.daljeet.xplayer.core.ui.theme.NextPlayerTheme
 
