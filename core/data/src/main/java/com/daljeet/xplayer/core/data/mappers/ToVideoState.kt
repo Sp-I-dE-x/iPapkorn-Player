3cb558ec40ec8cf09178ca158043d19a153c6c3a
Sp-I-dE-x <160456203+Sp-I-dE-x@users.noreply.github.com>
2024-07-26 12:30:37 +0530

    Update ToVideoState.kt

--- core/data/src/main/java/com/daljeet/xplayer/core/data/mappers/ToVideoState.kt
+++ core/data/src/main/java/com/daljeet/xplayer/core/data/mappers/ToVideoState.kt
@@ -1,7 +1,7 @@
 package com.daljeet.xplayer.core.data.mappers
 
 import com.daljeet.xplayer.core.data.models.VideoState
-import com.daljeet.xplayer.core.database.converter.UriListConverter
+import con.daljeet.xplayer.core.database.converter.UriListConverter
 import com.daljeet.xplayer.core.database.entities.MediumEntity
 
 fun MediumEntity.toVideoState(): VideoState {
@@ -11,6 +11,7 @@ fun MediumEntity.toVideoState(): VideoState {
         audioTrackIndex = audioTrackIndex,
         subtitleTrackIndex = subtitleTrackIndex,
         playbackSpeed = playbackSpeed,
-        externalSubs = UriListConverter.fromStringToList(externalSubs)
+        externalSubs = UriListConverter.fromStringToList(externalSubs),
+        videoScale = videoScale,
     )
 }
