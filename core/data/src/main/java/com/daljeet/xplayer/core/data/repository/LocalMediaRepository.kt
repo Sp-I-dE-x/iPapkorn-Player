45f470663bdaa47c150542aeefbdddc38e6e5b16
Sp-I-dE-x <160456203+Sp-I-dE-x@users.noreply.github.com>
2024-07-26 12:57:32 +0530

    Update LocalMediaRepository.kt

--- core/data/src/main/java/com/daljeet/xplayer/core/data/repository/LocalMediaRepository.kt
+++ core/data/src/main/java/com/daljeet/xplayer/core/data/repository/LocalMediaRepository.kt
@@ -1,28 +1,22 @@
-package com.daljeet.xplayer.core.data.repository
 
-import android.content.Context
+package dev.anilbeesetti.nextplayer.core.data.repository
+
 import android.net.Uri
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.IntentSenderRequest
-import dagger.hilt.android.qualifiers.ApplicationContext
-import com.daljeet.xplayer.core.common.di.ApplicationScope
-import com.daljeet.xplayer.core.common.extensions.deleteFiles
-import com.daljeet.xplayer.core.data.mappers.toFolder
-import com.daljeet.xplayer.core.data.mappers.toVideo
-import com.daljeet.xplayer.core.data.mappers.toVideoState
-import com.daljeet.xplayer.core.data.models.VideoState
-import com.daljeet.xplayer.core.database.converter.UriListConverter
-import com.daljeet.xplayer.core.database.dao.DirectoryDao
-import com.daljeet.xplayer.core.database.dao.MediumDao
-import com.daljeet.xplayer.core.database.entities.DirectoryEntity
-import com.daljeet.xplayer.core.database.entities.MediumEntity
-import com.daljeet.xplayer.core.database.relations.MediumWithInfo
-import com.daljeet.xplayer.core.model.Folder
-import com.daljeet.xplayer.core.model.Video
+import dev.anilbeesetti.nextplayer.core.common.di.ApplicationScope
+import dev.anilbeesetti.nextplayer.core.data.mappers.toFolder
+import dev.anilbeesetti.nextplayer.core.data.mappers.toVideo
+import dev.anilbeesetti.nextplayer.core.data.mappers.toVideoState
+import dev.anilbeesetti.nextplayer.core.data.models.VideoState
+import dev.anilbeesetti.nextplayer.core.database.converter.UriListConverter
+import dev.anilbeesetti.nextplayer.core.database.dao.DirectoryDao
+import dev.anilbeesetti.nextplayer.core.database.dao.MediumDao
+import dev.anilbeesetti.nextplayer.core.database.relations.DirectoryWithMedia
+import dev.anilbeesetti.nextplayer.core.database.relations.MediumWithInfo
+import dev.anilbeesetti.nextplayer.core.model.Folder
+import dev.anilbeesetti.nextplayer.core.model.Video
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -30,8 +24,7 @@ import timber.log.Timber
 class LocalMediaRepository @Inject constructor(
     private val mediumDao: MediumDao,
     private val directoryDao: DirectoryDao,
-    @ApplicationContext private val context: Context,
-    @ApplicationScope private val applicationScope: CoroutineScope
+    @ApplicationScope private val applicationScope: CoroutineScope,
 ) : MediaRepository {
 
     override fun getVideosFlow(): Flow<List<Video>> {
@@ -43,47 +36,36 @@ class LocalMediaRepository @Inject constructor(
     }
 
     override fun getFoldersFlow(): Flow<List<Folder>> {
-        return directoryDao.getAll().map { it.map(DirectoryEntity::toFolder) }
+        return directoryDao.getAllWithMedia().map { it.map(DirectoryWithMedia::toFolder) }
     }
 
-    override suspend fun getVideoState(path: String): VideoState? {
-        return mediumDao.get(path)?.toVideoState()
-    }
-
-    override suspend fun deleteVideos(videoUris: List<String>, intentSenderLauncher: ActivityResultLauncher<IntentSenderRequest>) {
-        val mediaUrisToDelete = videoUris.map { Uri.parse(it) }
-        context.deleteFiles(mediaUrisToDelete, intentSenderLauncher)
-    }
-
-    override suspend fun deleteFolders(folderPaths: List<String>, intentSenderLauncher: ActivityResultLauncher<IntentSenderRequest>) {
-        val mediumEntitiesToDelete = mutableListOf<MediumEntity>()
-        for (path in folderPaths) {
-            mediumEntitiesToDelete += mediumDao.getAllFromDirectory(path).first()
-        }
-        val mediaUrisToDelete = mediumEntitiesToDelete.map { Uri.parse(it.uriString) }
-        context.deleteFiles(mediaUrisToDelete, intentSenderLauncher)
+    override suspend fun getVideoState(uri: String): VideoState? {
+        return mediumDao.get(uri)?.toVideoState()
     }
 
     override suspend fun saveVideoState(
-        path: String,
+        uri: String,
         position: Long,
         audioTrackIndex: Int?,
         subtitleTrackIndex: Int?,
         playbackSpeed: Float?,
-        externalSubs: List<Uri>
+        externalSubs: List<Uri>,
+        videoScale: Float,
     ) {
         Timber.d(
-            "save state for [$path]: [$position, $audioTrackIndex, $subtitleTrackIndex, $playbackSpeed]"
+            "save state for [$uri]: [$position, $audioTrackIndex, $subtitleTrackIndex, $playbackSpeed]",
         )
 
         applicationScope.launch {
             mediumDao.updateMediumState(
-                path = path,
+                uri = uri,
                 position = position,
                 audioTrackIndex = audioTrackIndex,
                 subtitleTrackIndex = subtitleTrackIndex,
                 playbackSpeed = playbackSpeed,
-                externalSubs = UriListConverter.fromListToString(externalSubs)
+                externalSubs = UriListConverter.fromListToString(externalSubs),
+                lastPlayedTime = System.currentTimeMillis(),
+                videoScale = videoScale,
             )
         }
     }
