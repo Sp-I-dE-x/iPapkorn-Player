43b1175576f75bc28340a1d95f82af85498de439
Sp-I-dE-x <160456203+Sp-I-dE-x@users.noreply.github.com>
2024-07-26 12:53:00 +0530

    Update LocalPreferencesRepository.kt

--- core/data/src/main/java/com/daljeet/xplayer/core/data/repository/LocalPreferencesRepository.kt
+++ core/data/src/main/java/com/daljeet/xplayer/core/data/repository/LocalPreferencesRepository.kt
@@ -1,15 +1,16 @@
-package com.daljeet.xplayer.core.data.repository
 
-import com.daljeet.xplayer.core.datastore.datasource.AppPreferencesDataSource
-import com.daljeet.xplayer.core.datastore.datasource.PlayerPreferencesDataSource
-import com.daljeet.xplayer.core.model.ApplicationPreferences
-import com.daljeet.xplayer.core.model.PlayerPreferences
+package dev.anilbeesetti.nextplayer.core.data.repository
+
+import dev.anilbeesetti.nextplayer.core.datastore.datasource.AppPreferencesDataSource
+import dev.anilbeesetti.nextplayer.core.datastore.datasource.PlayerPreferencesDataSource
+import dev.anilbeesetti.nextplayer.core.model.ApplicationPreferences
+import dev.anilbeesetti.nextplayer.core.model.PlayerPreferences
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
 class LocalPreferencesRepository @Inject constructor(
     private val appPreferencesDataSource: AppPreferencesDataSource,
-    private val playerPreferencesDataSource: PlayerPreferencesDataSource
+    private val playerPreferencesDataSource: PlayerPreferencesDataSource,
 ) : PreferencesRepository {
     override val applicationPreferences: Flow<ApplicationPreferences>
         get() = appPreferencesDataSource.preferences
@@ -18,13 +19,13 @@ class LocalPreferencesRepository @Inject constructor(
         get() = playerPreferencesDataSource.preferences
 
     override suspend fun updateApplicationPreferences(
-        transform: suspend (ApplicationPreferences) -> ApplicationPreferences
+        transform: suspend (ApplicationPreferences) -> ApplicationPreferences,
     ) {
         appPreferencesDataSource.update(transform)
     }
 
     override suspend fun updatePlayerPreferences(
-        transform: suspend (PlayerPreferences) -> PlayerPreferences
+        transform: suspend (PlayerPreferences) -> PlayerPreferences,
     ) {
         playerPreferencesDataSource.update(transform)
     }
