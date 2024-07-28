1be590c80cbb915cf1b91ad842f37b3e295f492a
Sp-I-dE-x <160456203+Sp-I-dE-x@users.noreply.github.com>
2024-07-28 12:49:53 +0530

    Update DirectoryEntity.kt

--- core/database/src/main/java/com/daljeet/xplayer/core/database/entities/DirectoryEntity.kt
+++ core/database/src/main/java/com/daljeet/xplayer/core/database/entities/DirectoryEntity.kt
@@ -5,14 +5,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(
-    tableName = "directories"
+    tableName = "directories",
 )
 data class DirectoryEntity(
     @PrimaryKey
     @ColumnInfo(name = "path")
     val path: String,
     @ColumnInfo(name = "filename") val name: String,
-    @ColumnInfo(name = "media_count") val mediaCount: Int,
     @ColumnInfo(name = "last_modified") val modified: Long,
-    @ColumnInfo(name = "size") val size: Long
 )
